49551a527988146557f012b2f3990d6497866fd1
Hiroshi Yamasaki <ymskhrs@gmail.com>
2021-04-08 07:55:02 +0900

    Fix example.

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 require "tambo"
+include Tambo
 
-console = Tambo::Console.new
+console = Console.new
 width, height = console.size
-text = Tambo::Text.new("Hello World!", width / 2 - 7, height / 2)
+text = Text.new("Hello World!", width / 2 - 7, height / 2)
 console.clear
 console.write(text)
 console.show
 
 loop do
   event = console.poll_event
-  if event.key?(Tambo::KEY_ESC)
+
+  if event.key?(KEY_ESC)
     console.close
     break
   end
