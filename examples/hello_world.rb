f76d3b92542f005b3183dcf68e689b46ce283ff3
Hiroshi Yamasaki <ymskhrs@gmail.com>
2021-04-12 08:09:00 +0900

    Fix example

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -2,29 +2,31 @@
 
 require "tambo"
 
-console = Tambo::Console.new
-width, height = console.size
-x = width / 2 - 7
-y = height / 2
-x = 0
-y = 1
-text = Tambo::Text.new("Hello World!", x, y)
-console.write(text)
-console.show
+begin
+  console = Tambo::Console.new
+  width, height = console.size
+  x = width / 2 - 7
+  y = height / 2
+  text = Tambo::Text.new("Hello World!", x, y)
+  console.write(text)
+  console.show
 
-i = 0
+  i = 0
 
-loop do
-  event = console.poll_event
+  loop do
+    event = console.poll_event
 
-  if event.key?(Tambo::KEY_ESC)
-    text = Tambo::Text.new("Hello World! #{i}", x, y)
-    console.clear
-    console.write(text)
-    console.show
+    if event.key?(Tambo::KEY_ESC)
+      text = Tambo::Text.new("Hello World! #{i}", x, y)
+      console.clear
+      console.write(text)
+      console.show
+      console.beep
+      break if i > 3
+    end
+
+    i += 1
   end
-
-  i += 1
+ensure
+  console.close
 end
-
-console.close
